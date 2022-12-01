9e98b631e54af456b895c16beacd19701d0e7e5f
knafil <knafil@yahoo.fr>
2022-12-01 13:15:52 +0100

    premier commit

---
+++ src/main/java/com/example/tutogithubactions/TutoGithubActionsApplication.java
@@ -0,0 +1,27 @@
+package com.example.tutogithubactions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class TutoGithubActionsApplication {
+    @GetMapping("/welcome")
+    public String welcome(){
+        return "Welcome to GitHub Actions";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(TutoGithubActionsApplication.class, args);
+    }
+
+}
+/*echo "# github-actions-docker" >> README.md
+        git init
+        git add README.md
+        git commit -m "first commit"
+        git branch -M main
+        git remote add origin https://github.com/knafil/github-actions-docker.git
+        git push -u origin main*/
